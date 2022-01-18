e29eb10407f72c480de011355b31f1c40f0660f4
David E. Barrera <dbarrerap@live.com>
2022-01-18 16:23:15 -0500

    Protected Routes

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/products', [ProductsController::class, 'index']);
-// Route::post('/products', [ProductsController::class, 'store']);
-// Route::get('/products/{id}', [ProductsController::class, 'show']);
-
-Route::resource('products', ProductsController::class);
+// Public Routes
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/{id}', [ProductsController::class, 'show']);
 Route::get('/products/search/{name}', [ProductsController::class, 'search']);
+
+// Protected Routes
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::post('/products', [ProductsController::class, 'store']);
+    Route::put('/products/{id}', [ProductsController::class, 'update']);
+    Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
+});
