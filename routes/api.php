77b3dfafbc814e3d5391e42e38261ceb444b60b6
David E. Barrera <dbarrerap@live.com>
2022-01-18 14:22:23 -0500

    Product Controller + Methods

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,14 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', function() {
-    return Product::all();
-});
-Route::post('/products', function() {
-    return Product::create([
-        'name' => 'Product One',
-        'slug' => 'product-one',
-        'description' => 'Lorem ipsum',
-        'price' => 99.99
-    ]);
-});
+Route::get('/products', [ProductsController::class, 'index']);
+// Route::post('/products', function() {
+//     return Product::create([
+//         'name' => 'Product One',
+//         'slug' => 'product-one',
+//         'description' => 'Lorem ipsum',
+//         'price' => 99.99
+//     ]);
+// });
